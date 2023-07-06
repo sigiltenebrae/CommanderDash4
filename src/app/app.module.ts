ddc5f84027eab5e89710180021a62437f56e2a02
sigiltenebrae <ck6202@rit.edu>
2023-07-06 12:41:23 -0400

    created dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {FlexModule} from "@angular/flex-layout";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot([
+      {path: '', component: DashboardComponent}
+    ]),
+    BrowserModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
